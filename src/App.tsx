ecfff7f445e0ecc3149bf80d3d2ec8aafee50619
Rivit Not Vivit <jack.problems@protonmail.com>
2026-01-13 07:25:56 -0800

    added themed component with provider and consumer

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,30 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import StateCounter from './components/StateCounter';
-/* added code starts */
 import ItemComponent from './components/ItemComponent';
+/* added code starts */
+import {useState} from 'react'
+import ThemeContext from './components/ThemeContext';
+import ThemeConsumer from './components/ThemeConsumer';
 /* added code stops */
 
 function App() {
+  {/* added code starts */}
+  const [theme, setTheme] = useState<string>('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+  {/* added code stops  */}
+
   return (
     <div>
-      <StateCounter />
-      {/* added code starts */}
+      <StateCounter />      
       <ItemComponent />
+      {/* added code starts */}
+      <ThemeContext.Provider value={{ theme, toggleTheme }}>
+        <ThemeConsumer />
+      </ThemeContext.Provider>
       {/* added code stops  */}
     </div>  
   );
